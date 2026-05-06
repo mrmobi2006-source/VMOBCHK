aa0e4efe0dcc10afc7240f244576dd6951d11042
mrmobi2006-source <mrmobi2006@gmail.com>
2026-05-05 17:06:37 -0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,1 +1,15 @@
+<?php
+$config = [];
+$config['botToken'] = getenv('BOT_TOKEN') ?: 'YOUR_BOT_TOKEN';
+$config['adminID']  = getenv('ADMIN_ID') ?: 'YOUR_ADMIN_ID';
+$config['logsID']   = getenv('LOGS_CHANNEL_ID') ?: 'YOUR_LOGS_CHANNEL_ID';
+$config['timeZone'] = 'UTC';
+$config['anti_spam_timer'] = 15;
+$config['sk_keys'] = explode(',', getenv('SK_KEYS') ?: '');
 
+$config['db']['hostname'] = getenv('DB_HOST') ?: '127.0.0.1';
+$config['db']['username'] = getenv('DB_USERNAME') ?: 'root';
+$config['db']['password'] = getenv('DB_PASSWORD') ?: '';
+$config['db']['database'] = getenv('DB_DATABASE') ?: 'checkerbot';
+$config['db']['port']     = getenv('DB_PORT') ?: 3306;
+?>
