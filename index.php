2c2668451855578da7f051227d0f1f5a9d7c0848
mrmobi2006-source <mrmobi2006@gmail.com>
2026-05-05 17:11:31 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-
+<?php
+header("Content-Type: application/json");
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    require __DIR__ . "/main.php";
+} else {
+    echo json_encode(["status" => "running", "bot" => "online"]);
+}
+?>
