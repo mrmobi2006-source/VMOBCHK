56385d45a27acb64b481d4534532451d6d67d063
mrmobi2006-source <mrmobi2006@gmail.com>
2026-05-05 17:08:07 -0700

    Update bot.php

--- functions/bot.php
+++ functions/bot.php
@@ -1,1 +1,11 @@
-
+<?php
+function bot($method,$datas=[]){
+    global $config;
+    $ch = curl_init("https://api.telegram.org/bot{$config['botToken']}/$method");
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+    $res = curl_exec($ch);
+    if(curl_error($ch)) var_dump(curl_error($ch));
+    else return json_decode($res);
+}
+?>
