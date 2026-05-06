e29edada2d60248ba3438790a5ed709774293b08
mrmobi2006-source <mrmobi2006@gmail.com>
2026-05-05 17:33:33 -0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 $config = [];
-$config['botToken'] = getenv('BOT_TOKEN') ?: 'YOUR_BOT_TOKEN';
-$config['adminID']  = getenv('ADMIN_ID') ?: 'YOUR_ADMIN_ID';
-$config['logsID']   = getenv('LOGS_CHANNEL_ID') ?: 'YOUR_LOGS_CHANNEL_ID';
+$config['botToken'] = getenv('BOT_TOKEN') ?: '8701255967:AAEG87R8_d1hNJlohl1bNPMCLVuX65KrAG0';
+$config['adminID']  = getenv('ADMIN_ID') ?: '6154678499';
+$config['logsID']   = getenv('LOGS_CHANNEL_ID') ?: '-1003973814680';
 $config['timeZone'] = 'UTC';
 $config['anti_spam_timer'] = 15;
 $config['sk_keys'] = explode(',', getenv('SK_KEYS') ?: '');
