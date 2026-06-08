81c42e3d8276f882f3f9ec4240c9f94b865de5aa
mrmobi2006-source <mrmobi2006@gmail.com>
2026-06-08 03:11:58 +0100

    Update owner information in config.py

--- config.py
+++ config.py
@@ -3,8 +3,9 @@
 """
 import os
 
-# ضع Telegram ID الخاص بك هنا
+# معلومات المالك
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "xtt1x")
 
 CONFIG = {
     "BOT_TOKEN": os.getenv("BOT_TOKEN", ""),
