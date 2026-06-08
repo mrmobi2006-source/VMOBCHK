adcc3dc97edea7ce0dbfa3a4a9a0f0ab3d45bf60
mrmobi2006-source <mrmobi2006@gmail.com>
2026-06-08 02:42:46 +0100

    Update config.py with placeholder values

--- config.py
+++ config.py
@@ -4,8 +4,8 @@
 import os
 
 # Owner ID (ضع Telegram ID الخاص بك هنا)
-OWNER_ID = int(os.getenv("OWNER_ID", "6154678499"))
+OWNER_ID = int(os.getenv("OWNER_ID", "YOUR_TELEGRAM_ID"))
 
 CONFIG = {
-    "BOT_TOKEN": os.getenv("BOT_TOKEN", "8485477400:AAH--C6Z4nmeLwD-3UMm26W2af_gmp_1NmY"),
+    "BOT_TOKEN": os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN"),
 }
